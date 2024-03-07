c2b322b2c813960e46a91c075269886a1574be03
rachel-cummings <rachel_cummings@brown.edu>
2024-03-07 13:46:09 -0500

    Fix task 3, dammit

--- tests/test-example.r
+++ tests/test-example.r
@@ -17,5 +17,5 @@ test_that("This is an example test", {
   expect_unique(data = data, c(year, country))
   expect_range(data=data, gdpPercap, 0, 1e6)
   expect_values(data = data, continent, c("Asia", "Europe", "Africa", "Americas", "Oceania"))
-})
+}) 
 
