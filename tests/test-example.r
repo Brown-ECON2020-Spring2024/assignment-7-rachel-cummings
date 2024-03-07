cd2467a7dafe8a3311350b09a4f7835d8e6a82e5
rachel-cummings <rachel_cummings@brown.edu>
2024-03-06 21:59:50 -0500

    Finished Task 3

--- tests/test-example.r
+++ tests/test-example.r
@@ -1,5 +1,14 @@
+library(testdat)
+
+# Task 3, tests 1 and 3
 test_that("This is an example test", {
-  a <- 2
-  b <- 10
-  expect_equal(a * b, 20)
+  b <- summary(model)$coefficients["gdpPercap", "Estimate"]
+  expect_equal(b, 0.00076, tolerance = 0.01)
+  expect_equal(nrow(data),1704)
+
 })
+
+# Task 3, test 3
+expect_unique(data = data, c(year, country))
+expect_range(data=data, gdpPercap, 0, 1e6)
+expect_values(data = data, continent, c("Asia", "Europe", "Africa", "Americas", "Oceania"))
